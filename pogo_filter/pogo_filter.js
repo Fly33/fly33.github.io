76a030281300b2faf559cef9d0b17b39cf7595a7
Victor Vinogradov <fly@acm-server.ru>
2022-09-28 00:14:02 +0300

    data update; fixed fandom images

--- pogo_filter/pogo_filter.js
+++ pogo_filter/pogo_filter.js
@@ -163,7 +163,7 @@ function bulbapedia_image(bulbapedia_id, size) {
 }
 
 function fandom_image(fandom_id, size) {
-    return `https://static.wikia.nocookie.net/pokemongo/images/${fandom_id[0]}/${fandom_id}.png/revision/latest/smart/width/${size}/height/${size}`;
+    return `https://static.wikia.nocookie.net/pokemongo/images/${fandom_id[0]}/${fandom_id}.png/revision/latest/scale-to-width-down/${size}`;
 }
 
 function image(i) {
